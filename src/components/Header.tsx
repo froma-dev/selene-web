edca23d2c1acc37a5e326cfb250fbd57319778af
froma <github@froma.dev>
2025-03-10 13:38:47 +0100

    change: header to sticky header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -21,34 +21,36 @@ const Header = ({ logo, name }: HeaderProps) => {
   };
 
   return (
-    <header>
-      <div className="brand">
-        <img src={src} alt={`${alt} logo`} className="brand-image" />
-        {name ? <h1 className="brand-name">{name}</h1> : null}
-      </div>
-      <nav className="nav">
-        <ul className="nav-list">
-          <li className="nav-item">
-            <a
-              className="nav-link"
-              onClick={(e) => handleNavClick(e, "projects")}
-              href="#projects"
-            >
-              Projects
-            </a>
-          </li>
-          <li className="nav-item">
-            <a
-              className="nav-link"
-              onClick={(e) => handleNavClick(e, "connect")}
-              href="#connect"
-            >
-              Connect
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <div className="header-container sticky">
+      <header>
+        <div className="brand">
+          <img src={src} alt={`${alt} logo`} className="brand-image" />
+          {name ? <h1 className="brand-name">{name}</h1> : null}
+        </div>
+        <nav className="nav">
+          <ul className="nav-list">
+            <li className="nav-item">
+              <a
+                className="nav-link"
+                onClick={(e) => handleNavClick(e, "projects")}
+                href="#projects"
+              >
+                Projects
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link"
+                onClick={(e) => handleNavClick(e, "connect")}
+                href="#connect"
+              >
+                Connect
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
   );
 };
 
