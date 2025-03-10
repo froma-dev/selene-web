0458c18d52b329b939c2a9651f358fad4bfca6d0
froma <github@froma.dev>
2025-03-10 19:20:10 +0100

    add: footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import "@styles/Footer.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <img className="wavy-worm" src={"/wavy-worm.svg"} alt="wavy worm" />
+    </footer>
+  );
+};
+
+export default Footer;
