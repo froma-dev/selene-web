9528cc54cf2c135f03616aafa9e9211fd90a21c9
froma <github@froma.dev>
2025-03-16 21:15:11 +0100

    add: config variables

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,6 @@
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 const GRAPHQL_API_BASE_URL = import.meta.env.VITE_GRAPHQL_API_BASE_URL;
-
-export { API_BASE_URL, GRAPHQL_API_BASE_URL };
+const YT_API_KEY = import.meta.env.VITE_YT_API_KEY;
+const YT_PLAYLIST_ID = "PLSCA-Ke-fK16ymRBj4DOR8CVJJTf_cyiY";
+const YT_HANDLE_ID = "@selenecreates";
+export { API_BASE_URL, GRAPHQL_API_BASE_URL, YT_API_KEY, YT_PLAYLIST_ID, YT_HANDLE_ID };
